0239f2877a09df61cc548d5d28a57f7f4b3a8636
joshuabecker91 <joshua.becker91@gmail.com>
2022-09-07 19:37:51 -0700

    algo practice

---
+++ staircase.js
@@ -0,0 +1,16 @@
+
+
+
+
+function staircase(n) {
+    let spaces = ""
+    let stairs = ""
+    let loopCount = (n+1)
+    for(let i=1; i < loopCount; i++){
+        spaces = " ".repeat(loopCount-1-i)
+        stairs = spaces + "#".repeat(i)
+        console.log(stairs)
+    }
+}
+
+staircase(6)
