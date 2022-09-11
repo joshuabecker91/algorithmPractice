9900dd901ca166fe147bc3a798c4f59997cf2f92
joshuabecker91 <joshua.becker91@gmail.com>
2022-09-10 19:41:05 -0700

    updating fronts assignment

--- codingDojo/fronts.js
+++ codingDojo/fronts.js
@@ -11,16 +11,41 @@ class SLL { // The signly-linked list class itself
     }
 
     // All methods built into your class go here
+
+    // This adds a node to the front of the list
     addFront(value){
         var newNode = new SLLNode(value); // Create a new node containing this value
         newNode.next = this.head; // Connect the node to the list. the head of the list
         this.head = newNode; // Make this node the head of the list
         return this.head; // OR you can say return this
     }
+
+    // This removes a node from the front of the list
+    removeFront(){
+        if(this.head == null){ // edge case: If the list is empty, nothing to remove
+            return this.head;
+        }
+        var removedNode = this.head; // Have a variable that will hold the node we'll remove
+        this.head = removedNode.next; // Moves the head of the list to the 2nd node, which will become the new head
+        removedNode.next = null; // Redirect the removed node to null
+        return this.head;
+    }
+
+    front(){
+        console.log(this.head.value);
+        return this.head.value;
+    }
+
 }
 
 var mySLL = new SLL(); // Starts us off with an empty list 
 mySLL.addFront(18)
 mySLL.addFront(5)
-// mySLL.addFront(73)
-console.log(mySLL)
+mySLL.addFront(73)
+console.log(mySLL);
+// console.log(mySLL.head.next); // This would give us the second node
+
+mySLL.removeFront();
+console.log(mySLL);
+
+mySLL.front();
