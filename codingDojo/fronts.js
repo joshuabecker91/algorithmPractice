d61fcddcb3c4e5d97106e5b80252f3b8df7895a3
joshuabecker91 <joshua.becker91@gmail.com>
2022-09-10 18:16:58 -0700

    uploading fronts assignment

---
+++ codingDojo/fronts.js
@@ -0,0 +1,15 @@
+class SLLNode { //Singly-linked list node class
+    constructor(val){
+        this.value = val; //Holds value for this node
+        this.next = null; //Pointer to the next node
+    }
+    addFront()
+
+}
+
+class SLL { // The signly-linked list class itself
+    constructor(){ // Will start with no nodes
+        this.head = null; // Head points to first node
+    }
+}
+
