578fc9fcaa7bd6e05883226ff9b8048c15ce2508
joshuabecker91 <joshua.becker91@gmail.com>
2022-11-05 11:14:42 -0700

    algo practice

--- salesByMatch.js
+++ salesByMatch.js
@@ -10,26 +10,46 @@
 // then divide each by 2 to find whole number of pairs (math.floor)
 // sum all pairs to get total pair count 
 
+
 function sockMerchant(n, ar) {
     let hashmap = {}
     let count = 0;
     for(let i = 0; i < ar.length; i++){
-        let sock = ar[i]
-        if(hashmap[sock]){
-            hashmap[sock]++
-        } else {
-            hashmap[sock] = 1
-        }
-        // console.log(hashmap)
+        hashmap[ar[i]] ? hashmap[ar[i]]++ : hashmap[ar[i]] = 1
     }
-    // console.log("end", hashmap)
     for(let key in hashmap){
-        // console.log(hashmap[key])
         count += Math.floor(hashmap[key] / 2)
-        // console.log("count", count)
     }
-    // console.log(count)
     return count
 }
 
+
 sockMerchant(7, [1,2,1,2,1,3,2])
+
+
+
+
+
+// function sockMerchant(n, ar) {
+//     let hashmap = {}
+//     let count = 0;
+//     for(let i = 0; i < ar.length; i++){
+//         let sock = ar[i]
+//         if(hashmap[sock]){
+//             hashmap[sock]++
+//         } else {
+//             hashmap[sock] = 1
+//         }
+//         // console.log(hashmap)
+//     }
+//     // console.log("end", hashmap)
+//     for(let key in hashmap){
+//         // console.log(hashmap[key])
+//         count += Math.floor(hashmap[key] / 2)
+//         // console.log("count", count)
+//     }
+//     // console.log(count)
+//     return count
+// }
+
+// sockMerchant(7, [1,2,1,2,1,3,2])
