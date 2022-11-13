a779d234fe7d1a01692ee062716ebef761bab885
joshuabecker91 <joshua.becker91@gmail.com>
2022-11-12 16:48:56 -0800

    algo practice

--- leetCode/detectCapital.js
+++ leetCode/detectCapital.js
@@ -1,15 +1,20 @@
 
 
+function detectCapitalUse(word) {
+    return (
+        word === word.toUpperCase() ||
+        word === word.toLowerCase() ||
+        word === word[0].toUpperCase() + word.slice(1).toLowerCase()
+    );
+}
+
+
+
+
 var detectCapitalUse = function(word) {
-    if(word.toLowerCase() == word){
-        return true
-    }
-    else if(word.toUpperCase() == word){
-        return true
-    }
-    else if(word[0].toUpperCase() + word.slice(1,word.length).toLowerCase() == word){
-        return true
-    }
+    if(word.toLowerCase() == word) return true
+    else if(word.toUpperCase() == word) return true
+    else if(word[0].toUpperCase() + word.slice(1,word.length).toLowerCase() == word) return true
     return false
 };
 
@@ -17,8 +22,21 @@ detectCapitalUse("Google")
 
 
 
-
-
-
 // console.log(word[0].toUpperCase() + word.slice(1,word.length).toLowerCase())
 // console.log(word)
+
+
+// var detectCapitalUse = function(word) {
+//     if(word.toLowerCase() == word){
+//         return true
+//     }
+//     else if(word.toUpperCase() == word){
+//         return true
+//     }
+//     else if(word[0].toUpperCase() + word.slice(1,word.length).toLowerCase() == word){
+//         return true
+//     }
+//     return false
+// };
+
+// detectCapitalUse("Google")
