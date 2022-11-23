9636dcb357f7052f6d8c435d5e9d3cc7adb619b1
joshuabecker91 <joshua.becker91@gmail.com>
2022-11-22 19:33:29 -0800

    algo practice

--- codeSignal/arrayPreviousConversion.js
+++ codeSignal/arrayPreviousConversion.js
@@ -1,7 +1,19 @@
 
 
 function solution(items) {
-    
-
-
+    let newArray = [];
+    for (let i = 0; i < items.length; i++) {
+        let d = -1;
+        for (let j = i - 1; j >= 0; j--) {
+            if (items[j] < items[i]) {
+                d = items[j];
+                break;
+            }
+        }
+        newArray.push(d);
+    }
+    console.log(newArray)
+    return newArray;
 }
+
+solution([3, 5, 2, 4, 5])
