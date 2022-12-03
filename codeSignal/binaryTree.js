d454b150558e12e3840d0134b339ac3b3252fa9c
joshuabecker91 <joshua.becker91@gmail.com>
2022-12-03 13:20:42 -0800

    algo practice

---
+++ codeSignal/binaryTree.js
@@ -0,0 +1,39 @@
+
+
+//
+// Binary trees are already defined with this interface:
+// function Tree(x) {
+//   this.value = x;
+//   this.left = null;
+//   this.right = null;
+// }
+function solution(arg1) {
+
+}
+
+// Write the function solution which creates and returns the binary tree:
+
+// 3
+// /  \
+// 1      5
+//   /
+// 4
+// Clarifications
+
+// You should ignore the input arg1
+// This function should always return the same output
+
+// [execution time limit] 4 seconds (js)
+
+// [input] integer arg1
+
+// [output] tree.integer
+
+// [JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+console.log("This prints to the console when you Run Tests");
+return "Hello, " + name;
+}
---
+++ codeSignal/linkedList.js
@@ -0,0 +1,35 @@
+
+
+// Singly-linked lists are already defined with this interface:
+// function ListNode(x) {
+//   this.value = x;
+//   this.next = null;
+// }
+//
+function solution(arg1) {
+
+}
+
+
+// Write the function solution which creates and returns the linked list:
+
+// 1 -> 3 -> 5 -> 7
+// Clarifications
+
+// You should ignore the input arg1
+// This function should always return the same output
+// [execution time limit] 4 seconds (js)
+
+// [input] integer arg1
+
+// [output] linkedlist.integer
+
+// [JavaScript] Syntax Tips
+
+// Prints help message to the console
+// Returns a string
+function helloWorld(name) {
+    console.log("This prints to the console when you Run Tests");
+    return "Hello, " + name;
+}
+
